8b4a0d72bbc10901afbf3adb4a4e2cd87f0c98a3
GutoJJ <augusto-jung@educar.rs.gov.br>
2023-11-23 01:41:35 -0300

    Finalização navbar + tela de clientes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
-  
   devise_for :mecanicos
   devise_scope :mecanico do
     authenticated :mecanico do
       root 'main#home', as: :authenticated_root
+      get '/admin/register_mecanico', to: 'mecanicos#new', as: 'new_admin_mecanico'
+      post '/admin/register_mecanico', to: 'mecanicos#create', as: 'admin_mecanicos'
+      get '/clientes', to: 'clientes#index', as: 'clientes'
+      get '/home', to: 'main#home', as: 'main_home'
     end
-
-  get '/admin/register_mecanico', to: 'mecanicos#new', as: 'new_admin_mecanico'
-  post '/admin/register_mecanico', to: 'mecanicos#create', as: 'admin_mecanicos'
-
     unauthenticated do
       root 'devise/sessions#new', as: :unauthenticated_root
     end
-
     get '/mecanicos/sign_out' => 'devise/sessions#destroy'
   end
+  
 end
